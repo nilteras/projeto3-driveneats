01a88156526a85bc8a69572ebfbdf73b8237a218
nilteras <niltonrnascimento@hotmail.com>
2023-03-29 12:08:26 -0300

    versão com bordas verdes-java

---
+++ script.js
@@ -0,0 +1,55 @@
+
+function escolherCarne() {
+    document.getElementById('carne').style.borderColor = "#32B72F";
+    document.getElementById('frango').style.borderColor = "#FFFFFF";
+    document.getElementById('peixe').style.borderColor = "#FFFFFF";
+
+}
+function escolherFrango() {
+    document.getElementById('frango').style.borderColor = "#32B72F";
+    document.getElementById('carne').style.borderColor = "#FFFFFF";
+    document.getElementById('peixe').style.borderColor = "#FFFFFF";
+
+}
+function escolherPeixe() {
+    document.getElementById('peixe').style.borderColor = "#32B72F";
+    document.getElementById('frango').style.borderColor = "#FFFFFF";
+    document.getElementById('carne').style.borderColor = "#FFFFFF";
+
+}
+function escolherCoca() {
+    document.getElementById('coca').style.borderColor = "#32B72F";
+    document.getElementById('suco').style.borderColor = "#FFFFFF";
+    document.getElementById('agua').style.borderColor = "#FFFFFF";
+
+}
+function escolherSuco() {
+    document.getElementById('suco').style.borderColor = "#32B72F";
+    document.getElementById('coca').style.borderColor = "#FFFFFF";
+    document.getElementById('agua').style.borderColor = "#FFFFFF";
+
+}
+function escolherAgua() {
+    document.getElementById('agua').style.borderColor = "#32B72F";
+    document.getElementById('suco').style.borderColor = "#FFFFFF";
+    document.getElementById('coca').style.borderColor = "#FFFFFF";
+
+}
+function escolherSundae() {
+    document.getElementById('sundae').style.borderColor = "#32B72F";
+    document.getElementById('mousse').style.borderColor = "#FFFFFF";
+    document.getElementById('pudim').style.borderColor = "#FFFFFF";
+
+}
+function escolherMousse() {
+    document.getElementById('mousse').style.borderColor = "#32B72F";
+    document.getElementById('sundae').style.borderColor = "#FFFFFF";
+    document.getElementById('pudim').style.borderColor = "#FFFFFF";
+
+}
+function escolherPudim() {
+    document.getElementById('pudim').style.borderColor = "#32B72F";
+    document.getElementById('mousse').style.borderColor = "#FFFFFF";
+    document.getElementById('sundae').style.borderColor = "#FFFFFF";
+
+}
