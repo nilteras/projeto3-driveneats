18f8df30ca82f0c3529ab1a0768a8ca6eb01fa36
nilteras <niltonrnascimento@hotmail.com>
2023-03-30 11:52:45 -0300

    versão com icone adicionado

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ function escolherCarne() {
     document.getElementById('carne').style.borderColor = "#32B72F";
     document.getElementById('frango').style.borderColor = "#FFFFFF";
     document.getElementById('peixe').style.borderColor = "#FFFFFF";
+    document.getElementById('icon-carne').style.color = "#32B72F";
+    document.getElementById('icon-frango').style.color = "#FFFFFF";
+    document.getElementById('icon-peixe').style.color = "#FFFFFF";
     prato = "Carne";
 
 }
@@ -13,6 +16,9 @@ function escolherFrango() {
     document.getElementById('frango').style.borderColor = "#32B72F";
     document.getElementById('carne').style.borderColor = "#FFFFFF";
     document.getElementById('peixe').style.borderColor = "#FFFFFF";
+    document.getElementById('icon-frango').style.color = "#32B72F";
+    document.getElementById('icon-carne').style.color = "#FFFFFF";
+    document.getElementById('icon-peixe').style.color = "#FFFFFF";
     prato = "Frango";
 
 }
@@ -20,6 +26,9 @@ function escolherPeixe() {
     document.getElementById('peixe').style.borderColor = "#32B72F";
     document.getElementById('frango').style.borderColor = "#FFFFFF";
     document.getElementById('carne').style.borderColor = "#FFFFFF";
+    document.getElementById('icon-peixe').style.color = "#32B72F";
+    document.getElementById('icon-frango').style.color = "#FFFFFF";
+    document.getElementById('icon-carne').style.color = "#FFFFFF";
     prato = "Peixe";
 
 }
@@ -27,6 +36,9 @@ function escolherCoca() {
     document.getElementById('coca').style.borderColor = "#32B72F";
     document.getElementById('suco').style.borderColor = "#FFFFFF";
     document.getElementById('agua').style.borderColor = "#FFFFFF";
+    document.getElementById('icon-coca').style.color = "#32B72F";
+    document.getElementById('icon-suco').style.color = "#FFFFFF";
+    document.getElementById('icon-agua').style.color = "#FFFFFF";
     bebida = "Coca-Cola";
 
 }
@@ -34,6 +46,9 @@ function escolherSuco() {
     document.getElementById('suco').style.borderColor = "#32B72F";
     document.getElementById('coca').style.borderColor = "#FFFFFF";
     document.getElementById('agua').style.borderColor = "#FFFFFF";
+    document.getElementById('icon-suco').style.color = "#32B72F";
+    document.getElementById('icon-coca').style.color = "#FFFFFF";
+    document.getElementById('icon-agua').style.color = "#FFFFFF";
     bebida = "Suco";
     
 
@@ -42,6 +57,9 @@ function escolherAgua() {
     document.getElementById('agua').style.borderColor = "#32B72F";
     document.getElementById('suco').style.borderColor = "#FFFFFF";
     document.getElementById('coca').style.borderColor = "#FFFFFF";
+    document.getElementById('icon-agua').style.color = "#32B72F";
+    document.getElementById('icon-suco').style.color = "#FFFFFF";
+    document.getElementById('icon-coca').style.color = "#FFFFFF";
     bebida = "Agua";
 
 }
@@ -49,6 +67,9 @@ function escolherSundae() {
     document.getElementById('sundae').style.borderColor = "#32B72F";
     document.getElementById('mousse').style.borderColor = "#FFFFFF";
     document.getElementById('pudim').style.borderColor = "#FFFFFF";
+    document.getElementById('icon-sundae').style.color = "#32B72F";
+    document.getElementById('icon-mousse').style.color = "#FFFFFF";
+    document.getElementById('icon-pudim').style.color = "#FFFFFF";
     sobremesa = "Sundae";
 
 }
@@ -56,6 +77,9 @@ function escolherMousse() {
     document.getElementById('mousse').style.borderColor = "#32B72F";
     document.getElementById('sundae').style.borderColor = "#FFFFFF";
     document.getElementById('pudim').style.borderColor = "#FFFFFF";
+    document.getElementById('icon-mousse').style.color = "#32B72F";
+    document.getElementById('icon-sundae').style.color = "#FFFFFF";
+    document.getElementById('icon-pudim').style.color = "#FFFFFF";
     sobremesa = "Mousse";
 
 }
@@ -63,7 +87,9 @@ function escolherPudim() {
     document.getElementById('pudim').style.borderColor = "#32B72F";
     document.getElementById('mousse').style.borderColor = "#FFFFFF";
     document.getElementById('sundae').style.borderColor = "#FFFFFF";
+    document.getElementById('icon-pudim').style.color = "#32B72F";
+    document.getElementById('icon-mousse').style.color = "#FFFFFF";
+    document.getElementById('icon-sundae').style.color = "#FFFFFF";
     sobremesa = "Pudim";
 
 }
-
