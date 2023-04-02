caa6515fdcd2c7da18bfb99ab62e6e2af357495c
nilteras <niltonrnascimento@hotmail.com>
2023-04-02 18:58:50 -0300

    versão com fechar pedido habilitado

--- script.js
+++ script.js
@@ -1,95 +1,76 @@
-let prato;
-let bebida;
-let sobremesa;
+let conteudoPrato = null;
+let conteudoBebida = null;
+let conteudSobremesa = null;
 
-function escolherCarne() {
-    document.getElementById('carne').style.borderColor = "#32B72F";
-    document.getElementById('frango').style.borderColor = "#FFFFFF";
-    document.getElementById('peixe').style.borderColor = "#FFFFFF";
-    document.getElementById('icon-carne').style.color = "#32B72F";
-    document.getElementById('icon-frango').style.color = "#FFFFFF";
-    document.getElementById('icon-peixe').style.color = "#FFFFFF";
-    prato = "Carne";
 
-}
-function escolherFrango() {
-    document.getElementById('frango').style.borderColor = "#32B72F";
-    document.getElementById('carne').style.borderColor = "#FFFFFF";
-    document.getElementById('peixe').style.borderColor = "#FFFFFF";
-    document.getElementById('icon-frango').style.color = "#32B72F";
-    document.getElementById('icon-carne').style.color = "#FFFFFF";
-    document.getElementById('icon-peixe').style.color = "#FFFFFF";
-    prato = "Frango";
+function escolherPrato(prato, buscaTitulo){
+     
+    const divAnterior = document.querySelector('.menu-prato .selecionado');
 
-}
-function escolherPeixe() {
-    document.getElementById('peixe').style.borderColor = "#32B72F";
-    document.getElementById('frango').style.borderColor = "#FFFFFF";
-    document.getElementById('carne').style.borderColor = "#FFFFFF";
-    document.getElementById('icon-peixe').style.color = "#32B72F";
-    document.getElementById('icon-frango').style.color = "#FFFFFF";
-    document.getElementById('icon-carne').style.color = "#FFFFFF";
-    prato = "Peixe";
+    if (divAnterior !== null){
+        divAnterior.classList.remove('selecionado');
+    }
 
-}
-function escolherCoca() {
-    document.getElementById('coca').style.borderColor = "#32B72F";
-    document.getElementById('suco').style.borderColor = "#FFFFFF";
-    document.getElementById('agua').style.borderColor = "#FFFFFF";
-    document.getElementById('icon-coca').style.color = "#32B72F";
-    document.getElementById('icon-suco').style.color = "#FFFFFF";
-    document.getElementById('icon-agua').style.color = "#FFFFFF";
-    bebida = "Coca-Cola";
-
-}
-function escolherSuco() {
-    document.getElementById('suco').style.borderColor = "#32B72F";
-    document.getElementById('coca').style.borderColor = "#FFFFFF";
-    document.getElementById('agua').style.borderColor = "#FFFFFF";
-    document.getElementById('icon-suco').style.color = "#32B72F";
-    document.getElementById('icon-coca').style.color = "#FFFFFF";
-    document.getElementById('icon-agua').style.color = "#FFFFFF";
-    bebida = "Suco";
+    prato.classList.add('selecionado');
     
+    const descricaoTitulo = document.querySelector(buscaTitulo+' .titulo');
 
+    conteudoPrato = descricaoTitulo.innerHTML;
+    
+    verificaSelecao();
+    
+    
+    
+    
 }
-function escolherAgua() {
-    document.getElementById('agua').style.borderColor = "#32B72F";
-    document.getElementById('suco').style.borderColor = "#FFFFFF";
-    document.getElementById('coca').style.borderColor = "#FFFFFF";
-    document.getElementById('icon-agua').style.color = "#32B72F";
-    document.getElementById('icon-suco').style.color = "#FFFFFF";
-    document.getElementById('icon-coca').style.color = "#FFFFFF";
-    bebida = "Agua";
+function escolherBebida(bebida, buscaTitulo){
+    const divAnterior = document.querySelector('.menu-bebida .selecionado');
 
+    if (divAnterior !== null){
+        divAnterior.classList.remove('selecionado');
+    }
+   
+    bebida.classList.add('selecionado');
+
+    const descricaoTitulo = document.querySelector(buscaTitulo+' .titulo');
+
+    conteudoBebida = descricaoTitulo.innerHTML;
+
+    verificaSelecao();
+    
+   
+   
 }
-function escolherSundae() {
-    document.getElementById('sundae').style.borderColor = "#32B72F";
-    document.getElementById('mousse').style.borderColor = "#FFFFFF";
-    document.getElementById('pudim').style.borderColor = "#FFFFFF";
-    document.getElementById('icon-sundae').style.color = "#32B72F";
-    document.getElementById('icon-mousse').style.color = "#FFFFFF";
-    document.getElementById('icon-pudim').style.color = "#FFFFFF";
-    sobremesa = "Sundae";
+function escolherSobremesa(sobremesa, buscaTitulo){
+    const divAnterior = document.querySelector('.menu-sobremesa .selecionado');
 
+    if (divAnterior !== null){
+        divAnterior.classList.remove('selecionado');
+    }
+
+    sobremesa.classList.add('selecionado');
+
+    const descricaoTitulo = document.querySelector(buscaTitulo+' .titulo');
+
+    conteudSobremesa = descricaoTitulo.innerHTML;
+    
+    verificaSelecao();
+    
+    
 }
-function escolherMousse() {
-    document.getElementById('mousse').style.borderColor = "#32B72F";
-    document.getElementById('sundae').style.borderColor = "#FFFFFF";
-    document.getElementById('pudim').style.borderColor = "#FFFFFF";
-    document.getElementById('icon-mousse').style.color = "#32B72F";
-    document.getElementById('icon-sundae').style.color = "#FFFFFF";
-    document.getElementById('icon-pudim').style.color = "#FFFFFF";
-    sobremesa = "Mousse";
 
-}
-function escolherPudim() {
-    document.getElementById('pudim').style.borderColor = "#32B72F";
-    document.getElementById('mousse').style.borderColor = "#FFFFFF";
-    document.getElementById('sundae').style.borderColor = "#FFFFFF";
-    document.getElementById('icon-pudim').style.color = "#32B72F";
-    document.getElementById('icon-mousse').style.color = "#FFFFFF";
-    document.getElementById('icon-sundae').style.color = "#FFFFFF";
-    sobremesa = "Pudim";
+function verificaSelecao(){
+    if( conteudoPrato !== null){
+        if(conteudoBebida !== null){
+            if(conteudSobremesa !== null){
+                const divFinalizar = document.querySelector('.finalizar');
+                divFinalizar.classList.add('escondido');
 
+                const divFechar = document.querySelector('.fechar-pedido');
+                divFechar.classList.remove('escondido');
+
+            }
+        }
+    }
+    
 }
